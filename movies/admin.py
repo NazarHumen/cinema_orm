cfdad14cfd0ff2be4359f1f6cc531dffb52d8642
NazarHumen <nazargumen172@gmail.com>
2026-04-06 21:10:46 +0300

    feat(movies): register models in django admin for management

---
+++ movies/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from movies.models import Genre, Director, Movie, Review
+
+# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Director)
+admin.site.register(Movie)
+admin.site.register(Review)
